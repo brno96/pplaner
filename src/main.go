16b109b0f0e2492c99b32d34208dd4c1a9350260
noah brombach <noah.projekt@hotmail.com>
2019-03-22 16:02:12 +0100

    http serve

--- src/main.go
+++ src/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"log"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
 
 func main() {
-	fmt.Printf("hello Heroku\n")
+    http.HandleFunc("/", handler)
+    log.Fatal(http.ListenAndServe(":8080", nil))
 }
